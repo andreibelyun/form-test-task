7f150322241c8cfbf46eb701b63cd6b059038636
Andrey Belyun <andreibelyun@yandex.com>
2022-11-13 16:27:15 +0300

    feat: enable html5 validation, implement data sending and simple notification about the request status

--- src/index.js
+++ src/index.js
@@ -1,6 +1,37 @@
-import '@fortawesome/fontawesome-free/css/fontawesome.css';
-import '@fortawesome/fontawesome-free/css/brands.css';
-import '@fortawesome/fontawesome-free/css/solid.css';
-import './index.css';
+import "@fortawesome/fontawesome-free/css/fontawesome.css";
+import "@fortawesome/fontawesome-free/css/brands.css";
+import "@fortawesome/fontawesome-free/css/solid.css";
+import "./index.css";
 
-// document.querySelector('.form__button-text').textContent = 'Заказать';
+const form = document.querySelector(".form");
+const input = document.querySelector("#phone-input");
+
+const sendPhoneNumber = (number) => {
+  const fakeAPI = "https://jsonplaceholder.typicode.com";
+
+  return fetch(`${fakeAPI}/posts`, {
+    method: "POST",
+    body: JSON.stringify({
+      phone: number,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  });
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  sendPhoneNumber(input.value)
+    .then((res) => {
+      if (res.ok) {
+        alert("Ваш заказ успешно оформлен.");
+        input.value = "";
+      } else {
+        throw new Error("Something went wrong");
+      }
+    })
+    .catch((err) => {
+      alert("Произошла ошибка, попробуйте ещё раз.");
+    });
+});
--- webpack.config.js
+++ webpack.config.js
@@ -21,19 +21,6 @@ module.exports = {
         test: /\.css/,
         use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
-    //   {
-    //     test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-    //     use: [
-    //       {
-    //         loader: "file-loader",
-    //         options: {
-    //           name: "[name].[ext]",
-    //           outputPath: "../fonts/",
-    //           publicPath: "../static/fonts",
-    //         },
-    //       },
-    //     ],
-    //   },
     ],
   },
   plugins: [
